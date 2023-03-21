d37fc0e2b8d7bddace6289935d39dcc3cf7295fc
InesRM <107609883+InesRM@users.noreply.github.com>
2023-03-21 08:40:32 +0100

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'http-example';
+export class AppComponent implements OnInit {
+
+
+  ngOnInit():void {
+   console.log('inicio del proyecto')
+    this.getPosts();
+  }
+
+
+getPosts() {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response)=>console.log(response));
+  // .then(res => res.json())
+  // .then(data => console.log(data))
+
+}
+
 }
